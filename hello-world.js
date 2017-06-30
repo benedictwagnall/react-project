32bdff07a2e0f54d44d226859e8b708bcf5e20fc
Wagnall <lol95371@pc0088.cs.diamond.ac.uk>
2017-06-30 12:16:20 +0100

    Create HelloWorld.js

---
+++ hello-world.js
@@ -0,0 +1,18 @@
+
+//Basic class to demonstrate and learn the creation of componenets with React.
+
+var HelloWorld = React.createClass ({ 
+
+	render: function(){
+		
+		return (
+
+			<div>
+				Hello World!
+			</div>
+		)
+
+	}
+
+
+    });
