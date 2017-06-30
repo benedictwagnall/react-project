c514861ec7605c3df3cea9265b7214e5713e54c8
Wagnall <lol95371@pc0088.cs.diamond.ac.uk>
2017-06-30 12:28:30 +0100

    Update HelloWorld.js, added comments

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,7 @@
 
 //Basic class to demonstrate and learn the creation of componenets with React.
 
+//Saved in a var so that we can specify which element needs to be rendered in the DOM
 var HelloWorld = React.createClass ({ 
 
 	render: function(){
@@ -16,3 +17,8 @@ var HelloWorld = React.createClass ({
 
 
     });
+
+
+//Takes two args: Desired component-to-render, DOM node to render to.
+//Take the HelloWorld component and render it to the element with ID of app.
+ReactDOM.render( <HelloWorld />, document.getElementById('app'));
